88b1e64f3f64fb513a9b4bc3a7545d24335bafc9
Blair Jilek <blairjilek@gmail.com>
2024-12-08 17:26:52 -0600

    made some changes to app jsx or some groundwork

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,15 @@
 import {useState} from 'react';
 
-function App () {
-  
+const App = () => {
+  //anything I have in a use state goes at the top 
+
+//anything that is a useEffect 
+
+//custome js handle functions (onClicks, onSubmit, etc)
+
+
+  // your return is just your "html" you can use javascript in here by wrapping things in {}
+  // this is most common for conditions ex {foo === "bar" &&  <div></div>}
   return (
     <div>
       <h1>TO DO APP</h1>
